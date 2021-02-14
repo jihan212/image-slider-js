49214db1e9e098b45d6c1a775703a7850e77abcb
snapper <zihanjashim2121@gmail.comm>
2021-02-14 12:07:00 +0600

    Image slider

--- app.js
+++ app.js
@@ -24,11 +24,23 @@ const showImages = (images) => {
     div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
     div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
     gallery.appendChild(div)
+    toggleSpinner(false);
   })
 
 }
 
+const toggleSpinner = (show) => {
+  const spinner = document.getElementById("loading-spinner");
+  if ( show ){
+    spinner.classList.remove('d-none');
+  } else {
+    spinner.classList.add('d-none');
+  }
+}
+
+
 const getImages = (query) => {
+  toggleSpinner(true);
   fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
     .then(response => response.json())
     .then(data => showImages(data.hits))

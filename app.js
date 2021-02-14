2c785483245a2c73d55b0744486924c54eaa3877
snapper <zihanjashim2121@gmail.comm>
2021-02-14 09:49:35 +0600

    Image slider

--- app.js
+++ app.js
@@ -67,7 +67,8 @@ const createSlider = () => {
   document.querySelector('.main').style.display = 'block';
   // hide image aria
   imagesArea.style.display = 'none';
-  const duration = document.getElementById('duration').value || 1000 ;
+  const duration = document.getElementById('duration') && 1000 ;
+
   sliders.forEach(slide => {
     let item = document.createElement('div')
     item.className = "slider-item";
